4db93c706daf859c6655087be43459bfd73f72ae
hiltas <h_iltas@yahoo.com>
2025-03-03 09:19:18 +0000

    safari added and jdk 17 DONE

--- src/test/java/com/utilities/Driver.java
+++ src/test/java/com/utilities/Driver.java
@@ -66,6 +66,7 @@ public class Driver {
                     driverPool.get().manage().window().maximize();
                     driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
                     break;
+
             }
 
         }
